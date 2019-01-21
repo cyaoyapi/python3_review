8bdde474c834158acfafba95a34be6473dd059ac
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-21 04:34:17 +0000

    Return serie as a list instead display it

--- 004_fibonacci_function/fibonacci.py
+++ 004_fibonacci_function/fibonacci.py
@@ -5,18 +5,21 @@
 
 def fibo(n):
 	"""Display fibonacci serie number until the given number."""
+	series = []
 	a, b = 0, 1
 	while a <= n:
-		print(f"{a}", end=" ")
+		series.append(a)
 		a, b = b, a+b
-	print("")
+	return series
 
 # The progam begins here
 try:
 	given_number = int(input("Give an integer greater than 1 : "))
 	if given_number < 0:
 		raise ValueError("Error : the given value must be greater than 0!")
-	fibo(given_number)
+	for nb in fibo(given_number):
+		print(f"{nb}", end=" ")
+	print(" ")
 except ValueError:
 	print("Error : the given value must be an positive integer!")
 	exit("===Try again!===")
